e169633b721dd6c8a9420fb7f8d8d1292cb8dd17
Ratul <azammmgol@gmail.com>
2021-11-06 20:28:26 +0600

    making changes to fix the bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,32 +1,26 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { ChakraProvider, extendTheme, CSSReset } from "@chakra-ui/react";
-import { LightMode } from "@chakra-ui/color-mode";
+import { ChakraProvider, /* extendTheme */ CSSReset } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  useSystemColorMode: false,
-  initialColorMode: "light",
-  colors: {
-    primary: "#FF218D",
-    secondary: {
-      500: "#FFD800",
-    },
-  },
-});
+// const theme = extendTheme({
+//   useSystemColorMode: false,
+//   initialColorMode: "light",
+//   colors: {
+//     primary: "#FF218D",
+//     secondary: {
+//       500: "#FFD800",
+//     },
+//   },
+// });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <LightMode>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </LightMode>
-    </ChakraProvider>
-  </React.StrictMode>,
+  <ChakraProvider /* theme={theme} */>
+    <CSSReset />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ChakraProvider>,
   document.getElementById("root")
 );
